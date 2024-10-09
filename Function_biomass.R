7b9cb3dbb2255597452d930558e2ec25e905dddf
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-10-09 11:17:46 -0300

    Update Function_biomass.R

--- Function_biomass.R
+++ Function_biomass.R
@@ -26,13 +26,7 @@ import_biomass_rawdata <-  function(site = NULL){
 
   sites <- c("cj","bp","bc", "it", "fsf","fb")
   site <- match(site, sites)
-  source <- c ("cj"= "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUXNWvjRvthnn29O8zSDdCDzN7F3ds2oR37xbcExjdzCLMk2TiZBxuBQuCpnNS5g/pub?output=csv",
-               "bp"="https://docs.google.com/spreadsheets/d/e/2PACX-1vSKa8azcr4AD5GI0SG1ay27BVSHkzKAvHdZ0G_5tnzZkObrFy7kdCtLwiuSLPA_Bg/pub?output=csv",
-               "bc" = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSQpuHlnRP-dF01wjmY1jGIAZLQYfK6g2NX2cSPKj_TMV5IBHQwpJ-Fwjx0OPXVOQ/pub?output=csv",
-               "it"="https://docs.google.com/spreadsheets/d/e/2PACX-1vSC559eIIhyIKCLOtPrVnnrp3JqoXd6AtJX345fURYEWuJpTw3QgyFnIHOp2DKO6Q/pub?output=csv",
-               "fsf"= "https://docs.google.com/spreadsheets/d/e/2PACX-1vTfXCGR2nwhacCPwyN88NADG0g9KIH3KQ5KwO9luxWZz6YLlFTZIdrsLGAkGTHXoQ/pub?output=csv",
-               "fb"="https://docs.google.com/spreadsheets/d/e/2PACX-1vQH6MiSv8vZDf5yWIIumiHWGTN3H0NF9yZ26P4Kw4J6nyUNe73PwHCJXLX33GUK4A/pub?output=csv"
-  )
+  source <- c ()
   site <- source [site]
   site <- read.csv(site[1], row.names = 1,)
   colnames (site)<- c("D","Alt","Vol","Gen","Spp","Fam","Distri","Filo")
