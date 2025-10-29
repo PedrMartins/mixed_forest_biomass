16fd8f69dc984375d755f7102946dcfb19948267
Pedro Martins <pedro.rufino.martins@gmail.com>
2025-10-29 11:35:26 -0300

    forgotten save

--- iNEXT_rarefacao.R
+++ iNEXT_rarefacao.R
@@ -5,22 +5,18 @@ rarefacaoRichiness = iNEXT (all_sites, q = c(0,1,2))
 
 
 R_E_sites_mixedforest_by_ha <-  ggiNEXT (rarefacaoRichiness, facet.var = "Order.q") +
-  theme_minimal() +
+  theme_minimal(base_family = "serif") +
   facet_wrap(~Order.q, scale = "free", labeller = as_labeller(c(
     "0" = "Species richness",
     "1" = "Shannon diversity",
     "2" = "Simpson diversity"))) +
   labs(
-    title = "Rarefaction/Extrapolation Curves by Diversity Order",
     x = "Sample Size",
     y = "Estimated Diversity"
   )
 
-  theme(
-    strip.text = element_text(size = 12, face = "bold"),
-    plot.title = element_text(size = 14, face = "bold", hjust = 0.5)
-  )
-ggsave ("rarefação_Simpson.jpg", gSimpson)
+ggsave ("R_E_mixed_by_ha.jpg", R_E_sites_mixedforest_by_ha, width = 2000,
+         units = "px")
 
 
 
